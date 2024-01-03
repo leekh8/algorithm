fe9a749febc96ce60b6f9509636066d4a3001b58
leekh8 <125537212+leekh8@users.noreply.github.com>
2024-01-03 15:40:27 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,19 +1,33 @@
-import java.util.Scanner; 
+import java.io.*;
 
-public class Main{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-		int count = sc.nextInt();
-        
-for (int i = 1; i <= count; i++) {
-			for (int j = count; j > i; j--) {
-				System.out.print(" ");
-			}
-			for (int k = 0; k < i; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-}
-        sc.close();
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            int T = Integer.parseInt(br.readLine());
+
+            for (int i = 1; i <= T; i++) {
+                for (int j = T - i; j > 0; j--) {
+                    bw.write(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    bw.write("*");
+                }
+                bw.write("\n");
+            }
+
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (bw != null) bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
