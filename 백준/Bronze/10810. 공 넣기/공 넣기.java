f03e6d038e8fd70c864a76666df80ac76d63a4b1
leekh8 <125537212+leekh8@users.noreply.github.com>
2024-01-04 18:56:51 +0900

    [Bronze III] Title: 공 넣기, Time: 144 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[] baskets = new int[n + 1];
+            for (int a = 0; a < m; a++) {
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                int i = Integer.parseInt(st2.nextToken());
+                int j = Integer.parseInt(st2.nextToken());
+                int k = Integer.parseInt(st2.nextToken());
+
+                for (int b = i; b <= j; b++) {
+                    baskets[b] = k;
+                }
+            }
+            for (int c = 1; c <= n; c++) {
+                bw.write(baskets[c] + " ");
+            }
+
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (bw != null) bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
