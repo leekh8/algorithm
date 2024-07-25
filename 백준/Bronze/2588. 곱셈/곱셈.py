16a8a7452d059dee05dae6ea62c5a93226fe662c
leekh8 <125537212+leekh8@users.noreply.github.com>
2024-07-25 11:02:53 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,10 +1,7 @@
-A = int(input())
-B = int(input())
-B1 = int(B/100)
-B2 = int((B % 100)/10)
-B3 = int(B % 10)
-
-print(A*B3)
-print(A*B2)
-print(A*B1)
-print(A*B)
+import sys
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+a3 = b % 10
+a4 = int((b % 100 - a3) / 10)
+a5 = int(b / 100)
+print(a * a3, a * a4, a * a5, a * b, sep='\n')
