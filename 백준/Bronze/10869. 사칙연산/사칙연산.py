50db9d3632b6bd1b90fc5fed55cefb516f2b59d4
leekh8 <125537212+leekh8@users.noreply.github.com>
2024-07-25 10:17:19 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,6 +1,3 @@
+# A+B, A-B, A*B, A/B(몫), A%B(나머지)
 a, b = map(int, input().split())
-print(a+b)
-print(a-b)
-print(a*b)
-print(int(a/b))
-print(a % b)
+print(a + b, a - b, a * b, int(a / b), a % b, sep = '\n') 
